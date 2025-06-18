cac8b4437234d8053f6dcb7b8307d64c3a7d443d
wolf1276 <ahirsarkar2022@gmail.com>
2025-06-19 00:34:16 +0530

    datatypes

--- basic_01/datatype.js
+++ basic_01/datatype.js
@@ -1,10 +1,10 @@
 const id ="111"
 let name = "ahir"
 let age = "12"
-let temp = "null"
+let temp = null
 let isloggedin = false
 console.log(age)
 console.log(temp)
-
-
-
+console.log(typeof temp)
+let tempu = Number(temp);
+console.log(tempu)
