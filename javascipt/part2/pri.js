4af8951931b4bbaae9b29d1b0cf2d3fdcd7b94b1
wolf1276 <120487235+wolf1276@users.noreply.github.com>
2025-07-05 03:47:44 +0530

    okkkk

--- javascipt/part2/pri.js
+++ javascipt/part2/pri.js
@@ -1,2 +1,19 @@
 let balance = 130;
 console.log(typeof balance);
+
+let isactive = true;
+let isreallyactive = new Boolean(true); //not recomened
+
+//concept on undefined and null
+
+let first = null;
+let last = undefined;
+console.log(typeof first);
+console.log(typeof last);
+
+//method to write string
+let name = "ahir";
+let class1 = 12;
+
+let printing = `i am ${name} of ${class1}`;
+console.log(printing);
